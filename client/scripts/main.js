801a24a772a37069c2dcb681d28eb543bc28ffa9
Nick Thompson <ncthom91@gmail.com>
2013-06-07 16:15:03 -0400

    Tree heirarchy in angular

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -9,17 +9,29 @@ function LibraryController($scope) {
     include_docs: true,
     filter: 'hoax/meta',
     complete: function (err, response) {
-      console.log(response);
+
+      // Organize the response into a library heirarchy
+      var library = {};
+      response.results.forEach(function (track) {
+        track = track.doc;
+
+        var artist = track.artist
+          , album = track.album;
+
+        library[artist] = library[artist] || {};
+        library[artist][album] = library[artist][album] || [];
+        library[artist][album].push(track);
+
+        track.attachment = '/'
+          + name
+          + '/' 
+          + track.musicbrainz_trackid
+          + '/file.mp3';
+      });
+
+      // Bind it to the dom
       $scope.$apply(function () {
-        $scope.tracks = response.results.map(function (row) {
-          row.doc.attachment = '/'
-            + name
-            + '/' 
-            + row.doc.musicbrainz_trackid
-            + '/file.mp3';
-          return row.doc;
-        });
-        console.log($scope.tracks);
+        $scope.library = library;
       });
     }
   });

74c40724aba23bc86a430a4d499b6451e56d5fbc
Nick Thompson <ncthom91@gmail.com>
2013-06-07 15:03:29 -0400

    Start writing actual tests

--- test/hoax.js
+++ test/hoax.js
@@ -1,11 +1,20 @@
 
-var assert = require("assert")
+var path    = require('path')
+  , load    = require('./load').loadModule
+  , hoax    = load(path.resolve(__dirname, '../lib/index.js'))
+  , assert  = require('assert');
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+// Test fixtures
+var flow = path.resolve(__dirname, './fixtures/01 Uppermost - Flow.mp3')
+  , norm = path.resolve(__dirname, './fixtures/02 Uppermost - The Norm.mp3');
+
+describe('Core functionality', function () {
+  it('Should be able to read id3 tags with Mutagen', function () {
+    hoax.readTag(flow, function (err, data) {
+      assert.equal('Flow', data.title);
+      assert.equal('Uppermost', data.artist);
+      assert.equal('21f965c6-5463-44a7-9897-7d9536d2db86', data._id);
+    });
+  });
+});
+

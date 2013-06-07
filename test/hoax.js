80f1e9081b30f00c39730b0ce132544712c3cf0f
Nick Thompson <ncthom91@gmail.com>
2013-06-07 15:06:23 -0400

    Test readDir

--- test/hoax.js
+++ test/hoax.js
@@ -5,10 +5,12 @@ var path    = require('path')
   , assert  = require('assert');
 
 // Test fixtures
-var flow = path.resolve(__dirname, './fixtures/01 Uppermost - Flow.mp3')
+var fixtures = path.resolve(__dirname, './fixtures')
+  , flow = path.resolve(__dirname, './fixtures/01 Uppermost - Flow.mp3')
   , norm = path.resolve(__dirname, './fixtures/02 Uppermost - The Norm.mp3');
 
 describe('Core functionality', function () {
+
   it('Should be able to read id3 tags with Mutagen', function () {
     hoax.readTag(flow, function (err, data) {
       assert.equal('Flow', data.title);
@@ -16,5 +18,15 @@ describe('Core functionality', function () {
       assert.equal('21f965c6-5463-44a7-9897-7d9536d2db86', data._id);
     });
   });
+
+  it('Can glob a directory looking for .mp3 files', function () {
+    hoax.readDir(fixtures, function (err, data) {
+      assert.equal(2, data.length);
+      assert.equal(flow, data[0]);
+    });
+  });
+
+  it('Should be more modular')
+
 });
 

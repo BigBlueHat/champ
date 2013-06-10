2a4776cc0113af453ade4f34cb99417fe1968175
Nick Thompson <ncthom91@gmail.com>
2013-06-10 12:00:26 -0400

    Test saving a track to the db

--- test/champ.js
+++ test/champ.js
@@ -1,5 +1,6 @@
 
 var path    = require('path')
+  , Pouch   = require('pouchdb')
   , load    = require('./load').loadModule
   , champ   = load(path.resolve(__dirname, '../lib/index.js'))
   , expect  = require('chai').expect;
@@ -7,15 +8,31 @@ var path    = require('path')
 // Test fixtures
 var fixtures = path.resolve(__dirname, './fixtures')
   , flow = path.resolve(__dirname, './fixtures/01 Uppermost - Flow.mp3')
-  , norm = path.resolve(__dirname, './fixtures/02 Uppermost - The Norm.mp3');
+  , norm = path.resolve(__dirname, './fixtures/02 Uppermost - The Norm.mp3')
+  , testRemote = 'http://localhost:5984/champtests';
 
 // So we don't clutter the test results
 champ.utils.setVerbosity(0);
 
 describe('Core functionality', function () {
 
+  var db;
+
+  before(function (done) {
+    Pouch(testRemote, function (err, pouch) {
+      if (err) return done(err);
+      db = pouch;
+      done();
+    });
+  });
+
+  after(function (done) {
+    Pouch.destroy(testRemote, done);
+  });
+
   it('Should be able to read id3 tags with Mutagen', function (done) {
     champ.readTag(flow, function (err, data) {
+      if (err) return done(err);
       expect(data.title).to.equal('Flow');
       expect(data.artist).to.equal('Uppermost');
       expect(data._id).to.equal('21f965c6-5463-44a7-9897-7d9536d2db86');
@@ -25,6 +42,7 @@ describe('Core functionality', function () {
 
   it('Should read id3 tags and binary data in parallel', function (done) {
     champ.getTrackData(flow, function (err, metadata, b64string) {
+      if (err) return done(err);
       expect(metadata.title).to.equal('Flow');
       expect(metadata.artist).to.equal('Uppermost');
       expect(metadata._id).to.equal('21f965c6-5463-44a7-9897-7d9536d2db86');
@@ -33,8 +51,22 @@ describe('Core functionality', function () {
     });
   });
 
+  it('Should save two documents for each track', function (done) {
+    champ.getTrackData(flow, function (err, metadata, b64string) {
+      if (err) return done(err);
+      champ.pushTrack(db, metadata, b64string, function (err, res) {
+        if (err) return done(err);
+        expect(res).to.have.length(2);
+        expect(res[0].id).to.equal('m:21f965c6-5463-44a7-9897-7d9536d2db86');
+        expect(res[1].id).to.equal('21f965c6-5463-44a7-9897-7d9536d2db86');
+        done();
+      });
+    });
+  });
+
   it('Can glob a directory looking for .mp3 files', function (done) {
     champ.readDir(fixtures, function (err, data) {
+      if (err) return done(err);
       expect(data).to.have.length(2);
       expect(data[0]).to.equal(flow);
       done();

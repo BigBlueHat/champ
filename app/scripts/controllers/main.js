4fb676b6341f5b291e1cc63d3bcd289435633cf6
Nick Thompson <ncthom91@gmail.com>
2013-06-28 11:17:48 -0400

    Restructure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('poopApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
